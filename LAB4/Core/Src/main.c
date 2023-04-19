880c70788b5b0a7a15d9100552bdb7de28124037
jintanabp <jintanaboonpx@gmail.com>
2023-04-19 21:01:09 +0700

    Update main.c

--- LAB4/Core/Src/main.c
+++ LAB4/Core/Src/main.c
@@ -21,7 +21,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+// จินตนา
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
